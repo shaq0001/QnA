4b908afa390b8d03d9b4c455f58d06e212b9df1f
shaq0001 <shaq0001@gmail.com>
2015-05-09 19:10:54 -0500

    buil submit form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,8 @@ class HomeController < ApplicationController
   end
    def about
   end
+
+   def temp
+   	redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/about' => 'home#about'
 
+  post '/questions' => 'home#temp'
+
   #get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
