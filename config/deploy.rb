2d7280b61bfceb64b55e17998b0e73f9536dc942
shaq0001 <shaq0001@gmail.com>
2015-05-14 23:18:46 -0500

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+require "bundler/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/postgresql"
+load "config/recipes/nodejs"
+load "config/recipes/rbenv"
+load "config/recipes/check"
+
+server "172.31.0.203", :web, :app, :db, primary: true
+
+set :user, "deployer"
+set :application, "HU"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:shaq0001/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
